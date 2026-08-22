4ee576b439f6f97a29552cb2c64fc02be95f075b
younesharti002-crypto <younes.harti002@gmail.com>
2026-08-22 18:39:34 +0100

    M6: add WhatsApp reminder idempotency index

--- prisma/migrations/20260822183800_add_whatsapp_clinic_config/migration.sql
+++ prisma/migrations/20260822183800_add_whatsapp_clinic_config/migration.sql
@@ -7,3 +7,7 @@ ADD COLUMN "whatsappLanguageCode" TEXT NOT NULL DEFAULT 'fr';
 
 -- CreateIndex
 CREATE UNIQUE INDEX "clinics_whatsappPhoneNumberId_key" ON "clinics"("whatsappPhoneNumberId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "whatsapp_events_clinicId_appointmentId_eventType_key"
+ON "whatsapp_events"("clinicId", "appointmentId", "eventType");
